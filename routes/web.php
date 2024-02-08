8324fa285294278b1f3e0b6b5332a6c3a200d5b6
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-08 16:23:23 +0100

    first commit -> migration + seeder completati

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestPokedexController as PokedexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout.app');
-});
+Route::get('/', [PokedexController::class, 'index']);
